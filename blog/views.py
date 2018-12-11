f9b7f64bce28c0d31b285065efc5b5aeae1f50fa
hwanginbeom <rydn2002@gmail.com>
2018-12-11 19:56:29 +0900

    dd

--- blog/views.py
+++ blog/views.py
@@ -15,9 +15,7 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
-def post_new(request):
-    form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+
 
 def post_new(request):
     if request.method == "POST":
@@ -33,10 +31,27 @@ def post_new(request):
     return render(request, 'blog/post_edit.html', {'form': form})
 
 
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+
+
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
-        form = PostForm( instance=post)
+        form = PostForm(instance=post)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
